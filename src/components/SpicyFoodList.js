b02da0f316dab6dff02ac2df5a716b7f5e02d438
COllins <collinsmuiruri77@gmail.com>
2022-09-25 17:59:16 +0300

    Completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
-import { spicyFoods, getNewRandomSpicyFood } from "../data";
-
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
     console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray)
+
+  }
+  function removeFood(id){
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li onClick={()=>removeFood(food.id)} key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
-
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
@@ -22,5 +35,4 @@ function SpicyFoodList() {
     </div>
   );
 }
-
 export default SpicyFoodList;
